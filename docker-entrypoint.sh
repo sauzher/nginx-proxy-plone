9b2d5f318410040fb27b5377426152663e8acc02
alessandro.ceglie <alessandro.ceglie@uniba.it>
2021-06-11 17:19:37 +0200

    minor customization one proxypass for the whole portal server upstreamm section instead of direct vhosting reusing of some variables during sed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,8 @@ set -e
 sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" /etc/nginx/conf.d/default.conf
 sed -i "s/DOMAIN_NAME/${DOMAIN_NAME}/g" /etc/nginx/redirects.conf
 sed -i "s/DOMAIN_ALIASES/${DOMAIN_ALIASES}/g" /etc/nginx/conf.d/default.conf
-sed -i "s/BACKEND/${BACKEND}/" /etc/nginx/conf.d/default.conf
-sed -i "s/BACKEND_PORT/${BACKEND_PORT}/" /etc/nginx/conf.d/default.conf
-sed -i "s/SITE_ID/${SITE_ID}/" /etc/nginx/conf.d/default.conf
+sed -i "s/BACKEND_HOST/${BACKEND}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/BACKEND_PORT/${BACKEND_PORT}/g" /etc/nginx/conf.d/default.conf
+sed -i "s/SITE_ID/${SITE_ID}/g" /etc/nginx/conf.d/default.conf
 
 nginx -g "daemon off;"
